4a18757fc97ff8d1c9e90a6505fcc104b6ccf926
hcngdaniel <hcng2017@gmail.com>
2022-01-12 16:18:08 +0800

    Debug respeaker

--- scripts/core/respeaker/respeaker.py
+++ scripts/core/respeaker/respeaker.py
@@ -5,29 +5,51 @@ from mr_voice.msg import Voice
 
 
 class Respeaker:
-    def __init__(self, voice_topic="/voice/text", say_topic="/speaker/say"):
+    def __init__(self, voice_topic="/voice/text", say_topic="/speaker/say", audio_path_topic="/respeaker/audio_path"):
         self.voice_topic = voice_topic
         self.say_topic = say_topic
-
+        self.audio_path_topic = audio_path_topic
+        
         self.say_pub = rospy.Publisher(self.say_topic, String, queue_size=10)
         
         rospy.Subscriber(self.voice_topic, Voice, callback=self.__text_callback)
+        rospy.Subscriber(self.audio_path_topic, String, callback=self.__audio_path_callback)
         
-        self.voice = Voice()
-    
+        self.voice = [Voice(), ""]
+
     def __text_callback(self, msg):
-        self.voice = msg
-    
+        self.voice[0] = msg
+
+    def __audio_path_callback(self, msg):
+        self.voice[1] = msg
+
     def get_voice(self):
-        voice = self.voice.copy()
-        self.voice = Voice()
+        voice = self.voice[0]
+        self.voice = [Voice(), ""]
         return voice
-    
+
     def get_text(self):
-        text = self.voice.text
-        self.voice = Voice()
+        text = self.voice[0].text
+        self.voice = [Voice(), ""]
         return text
+
+    def get_path(self):
+        path = self.voice[1]
+        self.voice = [Voice(), ""]
+        return path
     
     def say(self, text):
         self.say_pub.publish(text)
+
+
+# usage
+if __name__ == "__main__":
+    rospy.init_node("asdf")
+    rospy.loginfo("Node started")
+    respeaker = Respeaker()
+    while not rospy.is_shutdown():
+        text = respeaker.get_text()
+        if text != "":
+            print(repr(text))
+        rospy.Rate(20).sleep()
 

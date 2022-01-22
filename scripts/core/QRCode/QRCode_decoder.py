b53a60247b22f51fbc10273cfa1644a914d82fc4
hcngdaniel <hcng2017@gmail.com>
2022-01-22 13:08:13 +0800

    QRCodeDecoder usage ok

--- scripts/core/QRCode/QRCode_decoder.py
+++ scripts/core/QRCode/QRCode_decoder.py
@@ -13,3 +13,18 @@ class QRCodeDecoder:
             return decode(img)
         if isinstance(img, str):
             return decode(cv2.imread(img))
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        success, frame = cap.read()
+        codes = QRCodeDecoder.decode(frame)
+        for code in codes:
+            pts = np.array([[point.x, point.y] for point in code.polygon])
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(frame, [pts], True, (0, 0, 255), thickness=1)
+            print(code.data.decode())
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(16) in [27, ord('q')]:
+            break

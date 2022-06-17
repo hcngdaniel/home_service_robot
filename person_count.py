020089f7fd11b6e2ff03a2c869c024311a410e1d
Fu510nYT <nathan.han1024@gmail.com>
2022-06-17 16:36:59 +0800

    added person count

---
+++ person_count.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+import cv2
+import torch
+from pytorch_models import *
+
+def callback_image(msg):
+    global _frame
+    _frame = CvBridge().imgmsg_to_cv2(msg, "bgr8")
+
+
+
+COCO_CLASSES = (
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+)
+
+if __name__ == "__main__":
+    rospy.init_node("Person_Countt")
+    rospy.loginfo("Person Count Node")
+
+    # Ros Messages
+
+    _frame = None
+    rospy.Subscriber("/camera/rgb/image_raw", Image, callback_image)
+    
+    # Initalizing Models
+    rcnn = FasterRCNN()
+    previous_x = 0
+    
+    people_count = 0
+    last_frame = []
+    
+    while not rospy.is_shutdown():
+        rospy.Rate(20).sleep()
+        if _frame is None: continue
+        
+        frame = _frame.copy()
+        frame = cv2.resize(frame, (1280, 960))
+        
+        # Visualize Lines
+        cv2.line(
+            frame, 
+            (frame.shape[1] // 3, 0), 
+            (frame.shape[1] // 3, frame.shape[0]), 
+            (255, 255, 0),
+            2
+        )
+        
+        cv2.line(
+            frame,
+            (frame.shape[1] // 3 * 2, 0),
+            (frame.shape[1] // 3 * 2, frame.shape[0]),
+            (255, 255, 0),
+            2
+        )
+        
+        
+        boxes = rcnn.forward(frame)
+        for _id, index, conf, x1, y1, x2, y2 in boxes:
+            if rcnn.labels[index] == "person":
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(frame, rcnn.labels[index], (x1 + 5, y1 + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1) 
+                
+                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+                
+                if previous_x < frame.shape[1] // 2 and cx > frame.shape[1] // 2:
+                    people_count += 1
+                if previous_x > frame.shape[1] // 2 and cx < frame.shape[1] // 2:
+                    people_count -= 1
+                
+                if people_count < 0: people_count = 0                
+                previous_x = cx
+        cv2.putText(frame, str(people_count), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 3)
+            
+        
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) in [27, ord('q')]:
+            break
+
+
---
+++ pytorch_models.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import os
+import cv2
+import numpy as np
+import torch
+from torchvision.models.detection import fasterrcnn_mobilenet_v3_large_320_fpn
+from torchvision.models.detection import keypointrcnn_resnet50_fpn
+
+
+
+COCO_CLASSES = (
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+)
+
+class TorchModel(object):
+    def __init__(self, torch_home: str) -> None:
+        if torch_home is None:
+            if "TORCHHUB_DIR" in os.environ:
+                torch_home = os.environ["TORCHHUB_DIR"]
+            else:
+                torch_home = "~/models/pytorch/hub"
+                
+        os.environ["TORCH_HOME"] = torch_home
+        self.device = "cpu"
+    
+    def BGR2Tensor(self, frame):
+        img = frame.copy()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = np.expand_dims(img.transpose(2, 0, 1), 0)
+        img = np.array(img, dtype=np.float32) / 255
+        img = torch.from_numpy(img)
+        img = img.to(self.device)
+        return img
+
+class Yolov5(TorchModel):
+    def __init__(self, torch_home: str = None) -> None:
+        super().__init__(torch_home)
+        self.net = torch.hub.load("ultralytics/yolov5", "yolov5s", device="cpu", force_reload=True)
+        self.net.eval().to(self.device)
+        self.labels = self.net.names
+
+    def forward(self, frame):
+        img = frame.copy()
+        out = self.net(img)
+
+        res = []
+        for x1, y1, x2, y2, pred, index in out.xyxy[0]:
+            if pred < 0.7: continue
+            x1, y1, x2, y2, index = map(int, (x1, y1, x2, y2, index))
+            res.append([0, index, pred, x1, y1, x2, y2])
+        return res
+
+class FasterRCNN(TorchModel):
+    def __init__(self, torch_home: str = None) -> None:
+        super().__init__(torch_home)
+        self.net = fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+        self.net.eval().to(self.device)
+        self.labels = COCO_CLASSES
+    
+    def forward(self, frame):
+        img = self.BGR2Tensor(frame)
+        out = self.net(img)
+
+        res = []
+        boxes = out[0]["boxes"]
+        labels = out[0]["labels"]
+        scores = out[0]["scores"]
+        for box, label, score in zip(boxes, labels, scores):
+            if score < 0.7: continue
+            x1, y1, x2, y2 = map(int, box)
+            res.append([0, label, score, x1, y1, x2, y2])
+        return res
+
+
